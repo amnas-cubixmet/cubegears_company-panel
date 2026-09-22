97b38fb056d81a20946e34b37a1f61d5aafbb96b
amnas-cubixmet <ali.cubimet@gmail.com>
2026-09-22 12:44:15 +0530

    remove guidance UI from dashboard layout

--- src/components/layout/DashboardLayout.jsx
+++ src/components/layout/DashboardLayout.jsx
@@ -3,9 +3,6 @@ import { Header } from './Header';
 import { Sidebar } from './Sidebar';
 import { MobileBottomNav } from './MobileBottomNav';
 import { Footer } from './Footer';
-import { GuidancePageAction } from '../../guidance/GuidancePageAction';
-import { GuidanceWindow } from '../../guidance/GuidanceWindow';
-import { DemoModeBanner } from '../../guidance/DemoModeBanner';
 
 export const DashboardLayout = ({ children }) => {
   return (
@@ -17,16 +14,13 @@ export const DashboardLayout = ({ children }) => {
 
         <main className="main-content scroll-hidden" style={{ padding: '24px', display: 'flex', flexDirection: 'column' }}>
           <div style={{ flex: 1 }}>
-            <DemoModeBanner />
-            <GuidancePageAction />
-            {children}
+{children}
           </div>
           <Footer />
         </main>
       </div>
 
       <MobileBottomNav />
-      <GuidanceWindow />
-    </div>
+</div>
   );
 };
