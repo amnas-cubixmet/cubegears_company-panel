084bb190f89d4472680a3805eb0e78688138c182
amnas-cubixmet <ali.cubimet@gmail.com>
2026-09-22 12:44:40 +0530

    remove guidance system: src/guidance/useGuidance.js

--- src/guidance/useGuidance.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import { GuidanceContext } from './GuidanceProvider';
-
-export function useGuidance() {
-  const context = useContext(GuidanceContext);
-  if (!context) throw new Error('useGuidance must be used inside GuidanceProvider');
-  return context;
-}
